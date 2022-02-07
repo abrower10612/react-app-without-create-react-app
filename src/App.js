6cb06fc284c4a971d1d6f7b2d40ff802c5fb0a7b
Andrew Brower <63933741+abrower10612@users.noreply.github.com>
2022-02-06 20:19:54 -0700

    add imported TestComponent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import TestComponent from "./components/TestComponent";
 
 class App extends React.Component {
     render() {
         return (
-            <h1>Hello World</h1>
+            <div>
+                <h1>Hello World</h1>
+                <TestComponent />
+            </div>
         )
     }
 }
---
+++ src/components/TestComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class TestComponent extends React.Component {
+    render() {
+        return (
+            <h2>Hello from imported TestComponent</h2>
+        )
+    }
+}
+
+export default TestComponent;
